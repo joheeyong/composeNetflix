96fa9e85662d509e3bcb20340e27f2d8b6311d1f
희용 <wh2dyd@naver.com>
2025-08-19 09:05:12 +0900

    compose with MVVM
    movie API 활용예정
    Flutter 프로젝트의 2번째 버전

---
+++ app/src/main/java/com/domeggook/composenetflix/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.domeggook.composenetflix
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.domeggook.composenetflix.ui.theme.ComposenetflixTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ComposenetflixTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ComposenetflixTheme {
+        Greeting("Android")
+    }
+}
